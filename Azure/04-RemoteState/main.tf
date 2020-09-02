bac481bc62d34c3084449a0361a8885b2741e03e
https://github.com/turnes <rafael@turnes.com.br>
2020-09-02 17:33:57 -0300

    How to use remote state

---
+++ Azure/04-RemoteState/main.tf
@@ -0,0 +1,28 @@
+terraform {
+  required_providers {
+    azurerm = {
+      source = "hashicorp/azurerm"
+      version = "~> 2.0" 
+    }    
+  }
+}
+
+# Configure the Azure provider
+provider "azurerm" {
+  subscription_id = var.subscription_id
+  features {}
+}
+
+# Create a new resource group
+resource "azurerm_resource_group" "rg" {
+  name     = var.rg_name
+  location = var.location
+  tags = var.tags
+}
+
+resource "azurerm_virtual_network" "vnet" {
+  name                = var.vnet_name
+  address_space       = var.vnet_address_space
+  location            = var.location
+  resource_group_name = azurerm_resource_group.rg.name
+}
